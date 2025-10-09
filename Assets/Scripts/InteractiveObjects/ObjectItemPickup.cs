3488287858b086d9af4ce4a7b45546173e3f294d
UtsumiEr1c3 <glhf2233@163.com>
2025-10-09 15:25:11 +0800

    feat: add item data and item object

---
+++ Assets/Scripts/InteractiveObjects/ObjectItemPickup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class ObjectItemPickup : MonoBehaviour
+{
+    private SpriteRenderer sr;
+
+    [SerializeField] private ItemDataSO itemData;
+
+    private void OnValidate()
+    {
+        if (itemData == null)
+        {
+            return;
+        }
+
+        sr = GetComponent<SpriteRenderer>();
+        sr.sprite = itemData.itemIcon;
+        gameObject.name = "Object ItemPickup - " + itemData.itemName;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Player pick up item: " + itemData.itemName);
+        Destroy(gameObject);
+    }
+}
